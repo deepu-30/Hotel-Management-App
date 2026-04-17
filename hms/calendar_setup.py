c5220079cc3fe035c91922d33bd8898ac9d48a3f
deepu-30 <80934348+deepu-30@users.noreply.github.com>
2026-04-17 22:46:51 +0530

    Add files via upload

---
+++ hms/calendar_setup.py
@@ -0,0 +1,19 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import pickle
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+def authenticate():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+    return creds
+
+if __name__ == "__main__":
+    print("Starting authentication...")
+    authenticate()
